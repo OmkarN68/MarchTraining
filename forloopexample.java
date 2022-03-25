8a8e15c67cc234340ed04c90ac4a7b117783f848
Omkar <omknagaral68@gmail.com>
2022-03-25 12:47:15 +0530

    examplefile

---
+++ forloopexample.java
@@ -0,0 +1,12 @@
+public class forloopexample {
+    public static void main(String[] args){
+        int n = 100;
+        int a = 0;
+
+        for (int i = 1;i<=100;++i)
+        {
+            a = a + i;
+        }
+        System.out.println(a);
+    }
+}
